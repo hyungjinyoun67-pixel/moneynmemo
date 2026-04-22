8b459c1c0344e0d621ee7d4fd85d406702332323
hyungjinyoun67-pixel <hyungjinyoun67@gmail.com>
2026-04-22 21:26:47 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-jsimport { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyCkV_2tbQyLj1KO3R5NGdEHdq1ntDh1f_c",
-  authDomain:        "moneynmemo.firebaseapp.com",
-  databaseURL:       "https://moneynmemo-default-rtdb.asia-southeast1.firebasedatabase.app
-",
-  projectId:         "moneynmemo",
-  storageBucket:     "moneynmemo.firebasestorage.app",
+  apiKey: "AIzaSyCkV_2tbQyLj1KO3R5NGdEHdq1ntDh1f_c",
+  authDomain: "moneynmemo.firebaseapp.com",
+  databaseURL: "https://moneynmemo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "moneynmemo",
+  storageBucket: "moneynmemo.firebasestorage.app",
   messagingSenderId: "320519694272",
-  appId:             "1:320519694272:web:6a3f6d0670fd9a2cce8a25",
+  appId: "1:320519694272:web:6a3f6d0670fd9a2cce8a25",
 };
 
 const app = initializeApp(firebaseConfig);
